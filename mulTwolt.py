b5f58db42b11f608ddba8803cf2fe0020c677994
toure1802 <jeunessefcm@gmail.com>
2020-07-15 01:55:40 -0400

    question12

--- mulTwolt.py
+++ mulTwolt.py
@@ -1,17 +1,24 @@
 import sys
-def mul(x,y):
-    return x*y
-if __name__=="__main__":
-        print(sys.argv)
-        if(len(sys.argv)==3):
-                x = int(sys.argv[1])
-                y = int(sys.argv[2])
-                print(mul(x,y))
-        elif(len(sys.argv)==2):
-                print("error")
-        elif(len(sys.argv)==1):
-                print("error")
-        else:
-                print("bye bye")
 
+def mul(a,b):
+	return (int(a*b))
+if __name__=="__main__":
+
+	print(sys.argv)
+
+	if(len(sys.argv)==3):
+    		x = int(sys.argv[1])
+    		y =int(sys.argv[2])
+    		print(mul(x,y))
+	elif(len(sys.argv)==2):
+    		x = int(sys.argv[1])
+    		y=int(input("veuillez entree la troixieme valeur:  "))   		
+    		print(mul(x,y))
+
+	elif(len(sys.argv)==1):
+    		x=int(input("veuillez entree la deuxieme valeur: "))
+    		y=int(input("veuillez entree la troixieme valeur: "))
+    		print(mul(x,y))
+	else:
+    		print("erreur")
 
